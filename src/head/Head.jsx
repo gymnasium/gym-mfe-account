85ff91a3935e41768d39aaa8cebff5ecbc70fd99
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-19 16:08:03 -0500

    add gym stuff

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -5,12 +5,17 @@ import { getConfig } from '@edx/frontend-platform';
 
 import messages from './messages';
 
+const config = getConfig();
+const settings = `${config.MARKETING_SITE_BASE_URL}/feeds/config.json`;
+const css = `${config.MARKETING_SITE_BASE_URL}/css/mfe.css`;
+
 const Head = ({ intl }) => (
   <Helmet>
     <title>
       {intl.formatMessage(messages['account.page.title'], { siteName: getConfig().SITE_NAME })}
     </title>
-    <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
+    <link rel="shortcut icon" href={config.FAVICON_URL} type="image/x-icon" />
+    <link rel="stylesheet" href={css} />
   </Helmet>
 );
 
